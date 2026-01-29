d4eb3a037bc717518aa0202f2854d6d22f499c5a
Nidhiee <96512591+Nidhiee@users.noreply.github.com>
2026-01-30 00:01:27 +0530

    Create train_model.py

---
+++ src/train_model.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+IMG_SIZE = 150
+BATCH_SIZE = 32
+
+train_dir = "data/chest_xray/train"
+val_dir = "data/chest_xray/val"
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='binary'
+)
+
+val_data = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='binary'
+)
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
+    MaxPooling2D(),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train_data, validation_data=val_data, epochs=5)
+
+model.save("models/pneumonia_model.h5")
+
+print("Model trained and saved successfully.")
